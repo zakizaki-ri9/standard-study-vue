d626aa90385921cfe32254cd1d5e301640fab176
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-27 08:21:20 +0900

    add hello vuejs

---
+++ hello_vuejs/main.js
@@ -0,0 +1,18 @@
+
+// #appがDOMとして読み込み完了したらVueインスタンスを生成するため、
+// "DOMContentLoaded"のイベントを待つ
+var app;
+document.addEventListener("DOMContentLoaded", () => {
+    app = new Vue({
+        el: "#app",
+        data: {
+            message: "Hello Vue.js !!",
+            list: ["apple", "orange", "grape"]
+        },
+        methods: {
+            addItemClick: function(event) {
+                this.list.push("newItem" + (this.list.length + 1))
+            }
+        }
+    })
+})
